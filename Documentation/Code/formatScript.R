70b9fc987dc4229ca0da34cb386c3295994c1b6a
jstef16 <jsteffan710@gmail.com>
2015-05-04 23:15:28 -0500

    Fixed code issues from final process audit
    Formatting issues in formatScript.R

--- Documentation/Code/formatScript.R
+++ Documentation/Code/formatScript.R
@@ -1,28 +1,28 @@
-#Name: formatScript
-#Creation Date: 3/10/2015
-#Author: Sean Lutjohn
-#Description: Formats the NSW data file so it can be joined to the dataset that has coordinates
-# Compares each LGA with the LGA's in the formatted file and if its a match, the unformatted file takes the 
+# Name: formatScript
+# Creation Date: 3/10/2015
+# Author: Sean Luthjohn
+# Description: Formats the NSW data file so it can be joined to the dataset that has coordinates
+# Compares each LGA with the LGA's in the formatted file and if it's a match, the unformatted file takes the 
 # name of the formatted file.
 
-data<- read.csv("C:/Users/Sean/Documents/GitHub/NICTA-NDSU-National-Map/Raw Data/LGAData - Complete.csv",fill = T,header = T)
+data <- read.csv("C:/Users/Sean/Documents/GitHub/NICTA-NDSU-National-Map/Raw Data/LGAData - Complete.csv", fill = T, header = T)
 data <- t(data)
 joinD <- read.csv("C:/Users/Sean/Desktop/joinTo.csv", fill =T, header = T)
 joinD$LGA_NAME11 <- as.character(joinD$LGA_NAME11)
 
-row <-1
-xx <-1
+row <- 1
+xx <- 1
 
 for(row in 3:dim(data)[1])
 {
   for(xx in 1:dim(joinD)[1])
   {
-    if(data[row,3] == substring(joinD[xx,2],1,nchar(joinD[xx,2])-4))
+    if(data[row,3] == substring(joinD[xx, 2], 1, nchar(joinD[xx, 2]) - 4))
     {
-      data[row,3] <- joinD[xx,2]
+      data[row, 3] <- joinD[xx, 2]
     }
   }
 }
 
-write.csv(t(data), file = "C:/Users/Sean/Google Drive/NICTA Capstone/NSW_DATA/NSWComplete.csv",row.names=FALSE)
+write.csv(t(data), file = "C:/Users/Sean/Google Drive/NICTA Capstone/NSW_DATA/NSWComplete.csv", row.names = FALSE)
  
--- formatScript.R
+++ formatScript.R
@@ -1,30 +1,30 @@
-#Name: formatScript
-#Creation Date: 3/10/2015
-#Author: Sean Lutjohn
-#Description: Formats the Integated NSW and Vicotria file so it can be joined to the dataset that has coordinates
-# Compares each LGA with the LGA's in the formatted file and if its a match, the unformatted file takes the 
+# Name: formatScript
+# Creation Date: 3/10/2015
+# Author: Sean Luthjohn
+# Description: Formats the Integrated NSW and Victoria file so it can be joined to the dataset that has coordinates
+# Compares each LGA with the LGA's in the formatted file and if it's a match, the unformatted file takes the 
 # name of the formatted file.
 
-data<- read.csv("Integrated.csv",fill = T,header = T)
+data <- read.csv("Integrated.csv", fill = T, header = T)
 data <- t(data)
 joinD <- read.csv("VIC_NSW.csv", fill =T, header = T)
 joinD$LGA_NAME11 <- as.character(joinD$LGA_NAME11)
 
-row <-1
-xx <-1
+row <- 1
+xx <- 1
 
 for(row in 3:dim(data)[1])
 {
   for(xx in 1:dim(joinD)[1])
   {
-    if(data[row,3] == substring(joinD[xx,2],1,nchar(joinD[xx,2])-4))
+    if(data[row, 3] == substring(joinD[xx, 2], 1, nchar(joinD[xx ,2]) -4))
     {
-      data[row,3] <- joinD[xx,2]
+      data[row, 3] <- joinD[xx, 2]
     }
   }
 }
 
 rownames(data)[1] <- ""
 
-write.table((data), file = "Integrated_NSW_VIC.csv",row.names=TRUE,col.names = FALSE,sep = ',')
+write.table((data), file = "Integrated_NSW_VIC.csv", row.names=TRUE, col.names = FALSE, sep = ',')
  
